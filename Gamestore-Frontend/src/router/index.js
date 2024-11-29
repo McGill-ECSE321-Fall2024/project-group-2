5e7cf2cddbd2ae80a8c56aa2a9a494a184247e35
katrinapanwar <katrina2panwar@gmail.com>
2024-11-29 15:41:31 -0500

    Updated HomeView.vue and BrowseView.vue

--- Gamestore-Frontend/src/router/index.js
+++ Gamestore-Frontend/src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import SignInView from '../views/SignInView.vue'; // Import the SignIn View
-import EmployeeView from '../views/EmployeeView.vue' // Ensure this view is imported
-import CustomerView from '../views/CustomerView.vue' // Ensure this view is imported
-import OwnerView from '../views/OwnerView.vue' // Ensure this view is imported
-import AccountView from '../views/AccountView.vue'
+import EmployeeView from '../views/EmployeeView.vue'; // Ensure this view is imported
+import CustomerView from '../views/CustomerView.vue'; // Ensure this view is imported
+import OwnerView from '../views/OwnerView.vue'; // Ensure this view is imported
+import AccountView from '../views/AccountView.vue';
+import BrowseView from '../views/BrowseView.vue'; // Import the Payment View
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-      {
-        path: '/account',  // Changed to a unique path for the games view
-        name: 'account',
-        // route level code-splitting
-        component: AccountView,
-      },
+    {
+      path: '/account', // Changed to a unique path for the account view
+      name: 'account',
+      // route level code-splitting
+      component: AccountView,
+    },
     {
       path: '/', // Default route for the home page
       name: 'home',
@@ -25,11 +26,26 @@ const router = createRouter({
       name: 'signin',
       component: SignInView,
     },
-    { path: "/customer", name: "customer", component: CustomerView },
-    { path: "/employees", name: "employee", component: EmployeeView },
-    { path: "/owner", name: "owner", component: OwnerView }
+    {
+      path: '/customer',
+      name: 'customer',
+      component: CustomerView
+    },
+    {
+      path: '/employees',
+      name: 'employee',
+      component: EmployeeView
+    },
+    {
+      path: '/owner',
+      name: 'owner',
+      component: OwnerView
+    },
+     { path: '/browse',
+     name: 'browse',
+     component: BrowseView
+     },
   ],
 });
 
 export default router;
-
